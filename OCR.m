84ddcb9e020568af6855d674a20cc1b495d2c929
thelu871 <therese.luong94@gmail.com>
2018-10-02 17:08:02 +0200

    New changes to localthresh

--- OCR.m
+++ OCR.m
@@ -1,16 +1,16 @@
 % Read the image and cast it to double
 % ====================================
-nuf = double(imread('nuf9.tif'));
+nuf = double(imread('nuf0b.tif'));
 % Extend the image
 % ================
 tmp = [nuf(:,64:-1:1) nuf nuf(:,128:-1:65)];
 nufextend = [tmp(64:-1:1,:); tmp; tmp(128:-1:65,:)];
 % Make a Gaussian filter
 % ======================
-sigma=10; %0.5
+sigma=10;
 lpH=exp(-0.5*([-64:64]/sigma).^2);
 lpH=lpH/sum(lpH); % Horizontal filter
-lpV=lpH';         % Vertical filter
+lpV=lpH'; % Vertical filter
 % Convolve in the horizontal and vertical direction
 % =================================================
 nufblur = conv2(nufextend, lpH, 'valid');
@@ -20,16 +20,19 @@ nufblur = conv2(nufblur, lpV, 'valid');
 nuf = nuf - nufblur + 128;
 figure(1)
 colormap(gray(256))
-
-subplot(2,2,1),imagesc(nufextend, [0 255])
+subplot(2,2,1),
+imagesc(nufextend, [0 255])
 axis image; colorbar
 title('extended image')
-
-axis image; title('Gaussian kernel')
-subplot(2,2,2), imagesc(nufblur, [0 255])
+subplot(2,2,2),
+plot(-64:64, lpH, '.-r')
+axis tight; title('Gaussian kernel')
+subplot(2,2,3), imagesc(nufblur, [0 255])
 axis image; colorbar
 title('blurred image image')
-
+subplot(2,2,4), imagesc(nuf, [0 255])
+axis image; colorbar
+title('new image')
 %{
 new_Im = zeros(128,128);
 for i = 1:128
@@ -48,6 +51,7 @@ axis image; colorbar
 title('new image thresh')
 %plot(-64:64, lpH, '.-r')
 %}
+%{
 subplot(2,2,4), imagesc(nuf, [0 255])
 axis image; colorbar
 title('new image')
@@ -55,9 +59,8 @@ title('new image')
 %%--------HISTOPART--------------------
 
 histo = hist(nuf(:),[0:255]);
-figure(2)
-colormap(gray)
 %{
+figure(2)
 bininvect=[0:1:255];
 histo=hist(nuf4b(:), bininvect);
 colormap(gray(256))
@@ -73,9 +76,9 @@ colormap(gray(256))
 subplot(2,3,1), imagesc(imTmid, [0 1]);
 axis image; colorbar
 title('least_error thresholded image');
-%}
-histo2 = hist(nuf(:),[0:255]);
 
+histo2 = hist(nuf(:),[0:255]);
+%}
 T2 = mean(mean(nuf))
 Tmid2 = mid_way(histo2, T)
 imTmid2 = nuf>Tmid2;
@@ -84,42 +87,31 @@ subplot(2,3,1), imagesc(imTmid2, [0 1]);
 axis image; colorbar
 title('mid-way thresholded image');
 
+CC=bwconncomp(imTmid2);
 
-CC=bwconncomp(~imTmid2);
-ImLabel=labelmatrix(CC);
-
-ImBig = 0*ImLabel;
+ImBig = 0*imTmid2;
 numPixels = cellfun(@numel,CC.PixelIdxList);
 [biggest,idx] = max(numPixels);
 ImBig(CC.PixelIdxList{idx}) = 1;
 subplot(2,3,2), imagesc(ImBig);
 axis image; title('ImBig'); colorbar
 
-%{
-keepBiggest=bwareafilt(~imTmid2,1);
-subplot(2,3,2), imagesc(keepBiggest);
-axis image; title('Keep biggest object'); colorbar
-%}
 
-%{
 imTmid_skel = bwmorph(imTmid,'skel',inf);
 subplot(2,3,3), imagesc(imTmid_skel, [0 1]);
 axis image; colorbar
 title('least-error skel');
-%}
 
-imTmid2_skel = bwmorph(ImBig,'skel',inf);
-subplot(2,3,3), imagesc(~imTmid2_skel, [0 1]);
+imTmid2_skel = bwmorph(imTmid2,'skel',inf);
+subplot(2,3,4), imagesc(imTmid2_skel, [0 1]);
 axis image; colorbar
 title('mid-way skel');
-%{
+
 subplot(2,3,5), imagesc(ocrdecide(imTmid_skel, 4), [0 1]);
 axis image; colorbar
 title('least-error orcdecide');
-%}
-ocrdecide(~imTmid2_skel,4)
-%{
-subplot(2,3,4), imagesc(ocrdecide(imInverse, 4));
+
+subplot(2,3,6), imagesc(ocrdecide(imTmid2_skel, 4), [0 1]);
 axis image; colorbar
 title('mid-way orcdecide');
 %}
